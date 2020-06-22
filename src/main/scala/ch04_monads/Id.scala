e56532ce994f9b35cc9578a0b4dcb931b125d83f
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2020-06-22 23:22:07 +0300

    Wrong understand task - reimplement methods for Id

--- src/main/scala/ch04_monads/Id.scala
+++ src/main/scala/ch04_monads/Id.scala
@@ -1,18 +1,11 @@
 package ch04_monads
 
-import ch04_monads.Monad
-
-// TODO:
-//  1. How to extends Monad?
-//  2. How to remove constructor param?
-case class Id[A](value: A) { // extends Monad[Id[A]] { ---> get error :(
-  import Id._
-
-  def flatMap[A, B](id: Id[A])(f: A => Id[B]): Id[B] = f(id.value)
-
-  def map[A, B](id: Id[A])(f: A => B): Id[B] = flatMap(id)(a => pure(f(a)))
-}
+import cats.Id
 
 object Id {
-  def pure[A](value: A): Id[A] = Id(value)
+  def pure[A](value: A): Id[A] = value
+
+  def map[A, B](initial: Id[A])(f: A => B): Id[B] = f(initial)
+  
+  def flatMap[A, B](initial: Id[A])(f: A => Id[B]): Id[B] = f(initial)
 }
