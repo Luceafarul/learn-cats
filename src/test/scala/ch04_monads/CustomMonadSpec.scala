7f467cf03bf18cb06668987763213599d4854c0a
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2020-07-02 21:22:02 +0300

    Add Option monad and Tree monad

---
+++ src/test/scala/ch04_monads/CustomMonadSpec.scala
@@ -0,0 +1,33 @@
+package ch04_monads
+
+import ch03_functors.Tree
+import ch03_functors.Tree._
+import org.scalatest.{WordSpec, Matchers}
+
+class CustomMonadSpec extends WordSpec with Matchers {
+  final case class Person(name: String, age: Int)
+
+  "TreeMonad" should {
+    "wrap value with pure method" in {
+      val person = Person("Marcus", 73)
+      val result = treeMonad.pure(person)
+
+      result shouldBe Leaf(person)
+    }
+
+    "transform tree content with flatMap" in {
+      val p1 = Person("Marcus", 73)
+      val p2 = Person("Antonii", 37)
+      val p3 = Person("Dekart", 23)
+      val persons: Tree[Person] = branch(branch(leaf(p1), leaf(p2)), leaf(p3))
+
+      val result = treeMonad.flatMap(persons)(p => leaf(p.name))
+
+      result shouldBe branch(
+        branch(leaf(p1.name), leaf(p2.name)),
+        leaf(p3.name)
+      )
+      result.isInstanceOf[Tree[String]] shouldBe true
+    }
+  }
+}
