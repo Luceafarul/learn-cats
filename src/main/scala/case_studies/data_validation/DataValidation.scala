a223cc50d1300d930ac3d8f1d2d383f9af40f04a
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2020-07-22 21:55:05 +0300

    Replace Either with Validated

--- src/main/scala/case_studies/data_validation/DataValidation.scala
+++ src/main/scala/case_studies/data_validation/DataValidation.scala
@@ -1,6 +1,9 @@
 package case_studies.data_validation
 
+import cats.data.Validated
+import cats.data.Validated._
 import cats.kernel.Semigroup
+import cats.syntax.apply._
 import cats.syntax.either._
 import cats.syntax.semigroup._
 
@@ -23,23 +26,17 @@ sealed trait Check[E, A] {
 
   def and(that: Check[E, A]): Check[E, A] = And(this, that)
 
-  def apply(a: A)(implicit s: Semigroup[E]): Either[E, A] = this match {
+  def apply(a: A)(implicit s: Semigroup[E]): Validated[E, A] = this match {
     case Pure(func) => func(a)
 
-    case And(left, right) =>
-      (left(a), right(a)) match {
-        case (Left(e1), Left(e2)) => (e1.combine(e2)).asLeft
-        case (Left(e), Right(_))  => e.asLeft
-        case (Right(_), Left(e))  => e.asLeft
-        case (Right(_), Right(_)) => a.asRight
-      }
+    case And(left, right) => (left(a), right(a)).mapN((_, _) => a)
   }
 }
 
 object Check {
   final case class And[E, A](left: Check[E, A], right: Check[E, A]) extends Check[E, A]
 
-  final case class Pure[E, A](func: A => Either[E, A]) extends Check[E, A]
+  final case class Pure[E, A](func: A => Validated[E, A]) extends Check[E, A]
 
-  def pure[E, A](f: A => Either[E, A]): Check[E, A] = Pure(f)
+  def pure[E, A](f: A => Validated[E, A]): Check[E, A] = Pure(f)
 }
--- src/test/scala/case_studies/data_validation/DataValidationSpec.scala
+++ src/test/scala/case_studies/data_validation/DataValidationSpec.scala
@@ -1,5 +1,7 @@
 package case_studies.data_validation
 
+import cats.data.Validated
+import cats.data.Validated._
 import cats.syntax.either._
 import cats.instances.list._
 import org.scalatest.{WordSpec, Matchers}
@@ -27,19 +29,19 @@ class DataValidationSpec extends WordSpec with Matchers {
   "Check" should {
     "validate and combine value" in {
       val a: Check[List[String], Int] = Check.pure { v =>
-        if (v > 2) v.asRight
-        else List("Must be > 2").asLeft
+        if (v > 2) Valid(v)
+        else Invalid(List("Must be > 2"))
       }
 
       val b: Check[List[String], Int] = Check.pure { v =>
-        if (v < -2) v.asRight
-        else List("Must be < -2").asLeft
+        if (v < -2) Valid(v)
+        else Invalid(List("Must be < -2"))
       }
 
       val check: Check[List[String], Int] = a.and(b)
 
-      check(5) shouldBe Either.left(List("Must be < -2"))
-      check(0) shouldBe Either.left(List("Must be > 2", "Must be < -2"))
+      check(5) shouldBe Invalid(List("Must be < -2"))
+      check(0) shouldBe Invalid(List("Must be > 2", "Must be < -2"))
     }
   }
 }
