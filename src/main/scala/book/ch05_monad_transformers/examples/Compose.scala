d3f6fc4b608ef53e2a1b9b81edfce8c6756c82d3
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2022-04-04 16:26:04 +0300

    Add examples and update transformers.

---
+++ src/main/scala/book/ch05_monad_transformers/examples/Compose.scala
@@ -0,0 +1,96 @@
+package book.ch05_monad_transformers.examples
+
+import cats.Monad
+import cats.syntax.applicative._
+
+import scala.util.{Success, Try}
+
+object Compose extends App {
+  // Hypothetical example. This won't actually compile:
+  def compose[M1[_] : Monad, M2[_] : Monad] = {
+    type Composed[A] = M1[M2[A]]
+    new Monad[Composed] {
+      def pure[A](a: A): Composed[A] = a.pure[M2].pure[M1]
+
+      def flatMap[A, B](fa: Composed[A])(f: A => Composed[B]): Composed[B] =
+        ???
+      // Problem! How do we write flatMap?
+      // We need to know extra knowledge about how to compose that monad with others
+
+      override def tailRecM[A, B](a: A)(f: A => Composed[Either[A, B]]): Composed[B] = ???
+    }
+  }
+
+  import cats.data.OptionT
+
+  type ListOption[A] = OptionT[List, A]
+
+  import cats.instances.list._
+  import cats.syntax.applicative._
+
+  val result1: ListOption[Int] = OptionT(List(Option(10)))
+  val result2: ListOption[Int] = 32.pure[ListOption]
+
+  val sumResult = result1.flatMap { x =>
+    result2.map { y =>
+      x + y
+    }
+  }
+  println(sumResult)
+  println(sumResult.value)
+
+  type ErrorOr[A] = Either[String, A]
+  type ErrorOrOption[A] = OptionT[ErrorOr, A]
+
+  val ten = 10.pure[ErrorOrOption]
+  val eleven = 11.pure[ErrorOrOption]
+
+  val twentyOne = ten.flatMap(x => eleven.map(_ + x))
+
+  println(twentyOne)
+  println(twentyOne.value)
+
+  import scala.concurrent.Future
+  import cats.data.EitherT
+
+  type FutureEither[A] = EitherT[Future, String, A]
+  type FutureEitherOption[A] = OptionT[FutureEither, A]
+
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  val futureEitherOr: FutureEitherOption[Int] =
+    for {
+      a <- 40.pure[FutureEitherOption]
+      b <- 37.pure[FutureEitherOption]
+    } yield a + b
+
+  futureEitherOr.value.value.onComplete(println)
+
+  import cats.data.Writer
+
+  type Logged[A] = Writer[List[String], A]
+
+  def parseNumber(s: String): Logged[Option[Int]] =
+    Try(s.toInt).toOption match {
+      case Some(num) => Writer(List(s"Read $s"), Some(num))
+      case None => Writer(List(s"Failed on $s"), None)
+    }
+
+  def addAll(a: String, b: String, c: String): Logged[Option[Int]] = {
+    import cats.data.OptionT
+    val result = for {
+      a <- OptionT(parseNumber(a))
+      b <- OptionT(parseNumber(b))
+      c <- OptionT(parseNumber(c))
+    } yield a + b + c
+    result.value
+  }
+
+  val resultOne = addAll("1", "2", "3")
+  val resultTwo = addAll("1", "a", "3")
+
+  println(resultOne)
+  println(resultOne.value)
+  println(resultTwo)
+  println(resultTwo.value)
+}
--- src/main/scala/book/ch05_monad_transformers/exercise/Transformers.scala
+++ src/main/scala/book/ch05_monad_transformers/exercise/Transformers.scala
@@ -3,38 +3,39 @@ package book.ch05_monad_transformers.exercise
 import cats.data.EitherT
 import cats.instances.future._
 import cats.syntax.applicative._
-import scala.concurrent.Await
-import scala.concurrent.Future
-import scala.concurrent.duration._
-import scala.concurrent.ExecutionContext.Implicits.global
 
-object Transformers {
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.Success
+
+object Transformers extends App {
 
   private val powerLevels = Map("Jazz" -> 6, "Bumblebee" -> 8, "Hot Rod" -> 10)
-  private val powerLevelForSpecialMove = 15
+  private val ComboPower = 15
 
   type Response[A] = EitherT[Future, String, A] // Future[Either[String, A]] -> EitherT[Future, String, A]
 
-  def powerLevel(autobot: String): Response[Int] = {
-    powerLevels.get(autobot) match {
+  def powerLevel(ally: String): Response[Int] = {
+    powerLevels.get(ally) match {
       case Some(value) => value.pure[Response]
-      case None        => EitherT.leftT(s"Common error: $autobot unreachable")
+      case None        => EitherT.leftT(s"Error: $ally unreachable")
     }
   }
 
-  def canSpecialMove(abot1: String, abot2: String): Response[Boolean] =
+  def canSpecialMove(ally1: String, ally2: String): Response[Boolean] =
     for {
-      pl1 <- powerLevel(abot1)
-      pl2 <- powerLevel(abot2)
-    } yield pl1 + pl2 > powerLevelForSpecialMove
+      pl1 <- powerLevel(ally1)
+      pl2 <- powerLevel(ally2)
+    } yield pl1 + pl2 > ComboPower
 
-  def tacticalReport(abot1: String, abot2: String): String = {
-    val stack = canSpecialMove(abot1, abot2).value
-
-    Await.result(stack, 1.seconds) match {
-      case Right(true)  => s"$abot1 and $abot2 are ready to roll out!"
-      case Right(false) => s"$abot1 and $abot2 need a recharge."
-      case Left(error)  => error
+  def tacticalReport(ally1: String, ally2: String): Unit =
+    canSpecialMove(ally1, ally2).value.onComplete {
+      case Success(Right(true))  => println(s"$ally1 and $ally2 are ready to roll out!")
+      case Success(Right(false)) => println(s"$ally1 and $ally2 need a recharge.")
+      case Success(Left(error))  => println(error)
     }
-  }
+
+  tacticalReport("Jazz", "Bumblebee")
+  tacticalReport("Bumblebee", "Hot Rod")
+  tacticalReport("Jazz", "Ironhide")
 }
